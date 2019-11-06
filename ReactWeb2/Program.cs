4a8b3e998c81fc9db663bdfef4d3e8496527d95a
slayer84vn@gmail.com <slayer84vn@gmail.com>
2019-11-06 08:59:54 +0700

    Sửa program

--- ReactWeb2/Program.cs
+++ ReactWeb2/Program.cs
@@ -15,6 +15,7 @@ namespace ReactWeb2
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
